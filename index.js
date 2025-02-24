2ff9cdb7a7eb45aac2bed3295a81baf085784e20
David Rios <drios9107@gmail.com>
2025-02-24 01:29:56 -0500

    integrated functionalities

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-// const f1 = require('./f1')
-
+require('dotenv').config()
 require('./utils/mongooseConnection')
 const express = require('express')
+const authRouter = require('./routes/auth')
 const categoryRouter = require('./routes/category')
 const subCategoryRouter = require('./routes/subCategory')
 const transactionRouter = require('./routes/transaction')
 const recurrentTransactionRouter = require('./routes/recurrentTransaction')
 const dashboardRouter = require('./routes/dashboard')
 const balanceRouter = require('./routes/balance')
+const { verifyToken } = require('./utils/middlewares')
 const app = express()
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cookieParser())
+app.use(verifyToken);
 
+app.use('/auth', authRouter)
 app.use('/categories', categoryRouter)
 app.use('/subCategories', subCategoryRouter)
 app.use('/recurrent-transactions', recurrentTransactionRouter)
 app.use('/transactions', transactionRouter)
 app.use('/dashboard', dashboardRouter)
 app.use('/balance', balanceRouter)
+
+
 app.listen(3001)
 
 // f1.logCurrentTimestamp()
