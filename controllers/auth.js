c757e0f7d5f32109c52bbb1bcf5c7594024b2761
David Rios <drios9107@gmail.com>
2025-05-13 20:46:54 -0400

    added auth error codes

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const { generateAccessToken } = require("../utils/common");
 
 exports.register = async (req, res) => {
     if (!req?.body?.email || !req?.body?.password)
-        return res.status(400).json({ status: 'error', message: 'Missing params' })
+        return res.status(400).json({ status: 'error', code: 'missing-params', message: 'Missing params' })
     else {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req?.body?.password, salt);
@@ -22,18 +22,18 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     try {
         if (!req?.body?.email || !req?.body?.password)
-            return res.status(400).json({ status: 'error', message: 'Missing params' })
+            return res.status(400).json({ status: 'error', code: 'missing-params', message: 'Missing params' })
         else {
             const response = await dbFunctions.find(userModel, { email: req.body.email });
             if (response?.status === 'error')
                 return res.status(500).json({ message: response })
 
             if (response.length === 0)
-                return res.status(404).json({ message: "The user doesn't exist" })
+                return res.status(404).json({ code: 'user-not-found', message: "The user doesn't exist" })
 
             const isValid = await bcrypt.compare(req.body.password, response[0].password);
             if (!isValid)
-                return res.status(403).json({ message: "The user/password combination is incorrect" })
+                return res.status(403).json({ code: 'invalid-credentials', message: "The user/password combination is incorrect" })
 
             const user = { email: response[0]?.email, _id: response[0]?._id?.toString() }
             const token = generateAccessToken(user)
@@ -42,19 +42,18 @@ exports.login = async (req, res) => {
         }
     } catch (error) {
         console.error('Login error:', error);
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ code: 'internal-server-error', message: 'Internal server error' });
     }
 }
 
 exports.verifyOauthAccessToken = async (req, res) => {
     try {
-
         if (!req?.body?.provider || !req?.body?.user || !req?.body?.accessToken)
             return res.status(400).json({ code: 'missing-params', message: 'Missing params' })
 
         const url = req.body.provider === 'github' ?
             "https://api.github.com/user" :
-            "https://www.googleapis.com/oauth2/v3/userinfo"
+            "https://www.googleapis.com/oauth2/v3/userinfo?access_token"
 
         const response = await fetch(url, {
             headers: { Authorization: `Bearer ${req.body.accessToken}` },
