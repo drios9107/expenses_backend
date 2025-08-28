431609f28f63a2932dd2a440f103f05064f739a0
David Rios <drios9107@gmail.com>
2025-08-28 11:11:22 -0400

    created default transaction values routes

---
+++ routes/defaultTransactionValue.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const controller = require('../controllers/defaultTransactionValue')
+const router = express.Router()
+
+router
+    .route('/:id')
+    .get(controller.getDetails)
+    .delete(controller.delete)
+    .put(controller.update)
+
+router
+    .route('/')
+    .post(controller.create)
+    .get(controller.getAll)
+
+
+module.exports = router
